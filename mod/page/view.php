1823928ffb4bee0637f87216de50af42e8bf9ebc
lichtbit <rostock@lichtbit.com>
2024-04-08 20:56:54 +0000

    Title nur anzeigen, wenn auch Beschreibung angezeigt werden soll

--- mod/page/view.php
+++ mod/page/view.php
@@ -59,6 +59,11 @@ $PAGE->set_url('/mod/page/view.php', array('id' => $cm->id));
 $options = empty($page->displayoptions) ? [] : (array) unserialize_array($page->displayoptions);
 
 $activityheader = ['hidecompletion' => false];
+
+if (empty($options['printintro'])) {
+    $activityheader['title'] = '';
+}
+
 if (empty($options['printintro'])) {
     $activityheader['description'] = '';
 }
